b3de24f91701b753e59167dbe5f045d1d42830a7
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-04 10:42:46 -0800

    Completed MVP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,95 @@
-// Write your "actions" router here!
+const express = require("express");
+const Actions = require("./actions-model");
+
+const router = express.Router();
+
+const validateId = (req, res, next) => {
+  const { id } = req.params;
+  Actions.get(id).then((actionID) => {
+    if (actionID) {
+      req.id = id;
+      next();
+    } else {
+      res.status(404).json({ message: `user with id ${id} not found` });
+    }
+  });
+};
+
+const validateAction = (req, res, next) => {
+  if (!req.body.project_id || !req.body.description || !req.body.notes) {
+    res.status(400).json({ message: "Missing required information." });
+  } else {
+    next();
+  }
+};
+
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.get("/:id", validateId, (req, res) => {
+  const { id } = req.params;
+
+  Actions.get(id)
+    .then((actID) => {
+      if (actID) {
+        res.status(200).json(actID);
+      } else {
+        res.status(404).json({ message: "The requested ID does not exist" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.post("/", validateAction, (req, res) => {
+  const newAction = req.body;
+
+  Actions.insert(newAction)
+    .then((actions) => {
+      res.status(201).json(actions);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.put("/:id", validateId, validateAction, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Actions.update(id, changes)
+    .then((updatedAct) => {
+      if (updatedAct) {
+        res.status(201).json(updatedAct);
+      } else {
+        res
+          .status(400)
+          .json({ message: `The requested action doesn't exist.` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.delete("/:id", validateId, (req, res) => {
+  Actions.remove(req.params.id)
+    .then((removed) => {
+      if (removed) {
+        res.status(200).json({ message: "Item Removed." });
+      } else {
+        res
+          .status(404)
+          .json({ message: "There was an issue removing the item." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,107 @@
-// Write your "projects" router here!
+const express = require("express");
+const Projects = require("./projects-model");
+const router = express.Router();
+
+const validateId = (req, res, next) => {
+  const { id } = req.params;
+  Projects.get(id).then((projID) => {
+    if (projID) {
+      req.id = id;
+      next();
+    } else {
+      res.status(404).json({ message: `user with ID ${id} not found` });
+    }
+  });
+};
+
+const validateProject = (req, res, next) => {
+  if (!req.body.name || !req.body.description) {
+    res.status(400).json({ message: "Missing required information." });
+  } else {
+    next();
+  }
+};
+
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+router.get("/:id/actions", validateId, (req, res) => {
+  const { id } = req.params;
+
+  Projects.getProjectActions(id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+router.get("/:id", validateId, (req, res) => {
+  const { id } = req.params;
+
+  Projects.get(id)
+    .then((projID) => {
+      if (projID) {
+        res.status(200).json(projID);
+      } else {
+        res.status(404).json({ message: "The requested ID does not exist" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.post("/", validateProject, (req, res) => {
+  Projects.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.put("/:id", validateId, validateProject, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  Projects.update(id, changes)
+    .then((updatedProj) => {
+      if (updatedProj) {
+        res.status(201).json(updatedProj);
+      } else {
+        res
+          .status(400)
+          .json({ message: "There was an issue updating the project." });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+router.delete("/:id", validateId, (req, res) => {
+  const { id } = req.params;
+
+  Projects.remove(id)
+    .then((test) => {
+      if (test) {
+        res.status(200).json({ message: "Project removed." });
+      } else {
+        res
+          .status(404)
+          .json({ message: "We were unable to remove the project." });
+      }
+    })
+    .catch((error) => {
+      res.status().json({ error: error.message });
+    });
+});
+
+module.exports = router;
